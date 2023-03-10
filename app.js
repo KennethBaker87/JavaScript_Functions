ab419ec7dd66765473432eee174c3e02705ed9a1
Kenneth Baker <spyksaturn@gmail.com>
2023-03-10 14:01:26 -0500

    initial commit

--- app.js
+++ app.js
@@ -2,6 +2,77 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+function printOdds(count){
+for (i = 1; i <= count; i++){
+    if (i % 2 != 0){
+        console.log(i)
+    }
+
+}
+}
+printOdds(30)
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+function checkAge(userName, age){
+    aboveSixteen = `Congrats ${userName}, you can drive!`
+    belowSixteen = `Sorry ${userName}, but you need to wait until you're 16.`
+    if (age >= 16){
+        console.log(aboveSixteen)
+    }
+    else{
+    console.log(belowSixteen)
+    }
+}
+checkAge(`Kenny`, 35)
+
+// Exercise 3 Section
+console.log("EXERCISE 3:\n==========\n");
+function caretsianPlane(x, y){
+    if(x > 0 && y > 0){
+        console.log(`Quadrant I`)
+    }
+    else if (x < 0 && y > 0){
+        console.log(`Quadrant II`)
+    }
+    else if (x < 0 && y < 0){
+        console.log(`Quadrant III`)
+    }
+    else if (x > 0 && y < 0){
+        console.log(`Quadrant (IV)`)
+    }
+    else if (x == 0 & y != 0){
+        console.log(`x axis`)
+    }
+    else if ( x != 0 && y == 0){
+        console.log(`y axis`)
+    }
+    else if(x == 0 && y == 0){
+        console.log(`center`)
+    }
+}
+caretsianPlane(3, -1)
+
+// Exercise 4 Section
+console.log("EXERCISE 4:\n==========\n");
+function validTriangle(side1, side2, side3){
+    let firstCheck = side1 + side2
+    let secondCheck = side1 + side3
+    let thirdCheck = side2 + side3
+    if(side3 > firstCheck || side2 > secondCheck || side1 > thirdCheck){
+        console.log(`invalid triangle`)
+    } 
+    else if (side1 == side2 && side1 == side3 && side2 == side3){
+        console.log(`equilateral triangle`)
+    }
+    else if (side1 == side2 || side1 == side3 || side2 == side3){
+        console.log (`isosceles triangle`)
+    }
+    else if (side1 != side2 && side1 != side3 && side2 != side3){
+        console.log(`scalene triangle`)
+    }
+}
+validTriangle(31, 1, 1)
+
+// Exercise 5 Section
+console.log("EXERCISE 5:\n==========\n");
